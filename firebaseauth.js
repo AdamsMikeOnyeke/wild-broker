a16ad9da641527a60ca0f76544b71d52f38b276a
Onyeke Adams Mike <onyekegodstime@gmail.com>
2025-06-08 20:52:57 +0100

    Added a login, signup and user dashboard

---
+++ firebaseauth.js
@@ -0,0 +1,18 @@
+  // Import the functions you need from the SDKs you need
+  import { initializeApp } from "https://www.gstatic.com/firebasejs/11.9.0/firebase-app.js";
+  // TODO: Add SDKs for Firebase products that you want to use
+  // https://firebase.google.com/docs/web/setup#available-libraries
+
+  // Your web app's Firebase configuration
+  const firebaseConfig = {
+    apiKey: "AIzaSyBMrstA_dagyNtGeM5iG13sUqH17tVuF2s",
+    authDomain: "broker-a8764.firebaseapp.com",
+    projectId: "broker-a8764",
+    storageBucket: "broker-a8764.firebasestorage.app",
+    messagingSenderId: "619267974730",
+    appId: "1:619267974730:web:f034b958f0237b20a568b3"
+  };
+
+  // Initialize Firebase
+  const app = initializeApp(firebaseConfig);
+
